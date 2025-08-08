26ab185410e68f48990bdde42505745f5602e6d1
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2025-08-08 09:12:48 -0700

    removed hf model clone

--- src/notebooks/04_scgpt_czi_finetuning_peft.py
+++ src/notebooks/04_scgpt_czi_finetuning_peft.py
@@ -58,13 +58,13 @@ sys.path.insert(0, "../")
 import scgpt as scg
 from scgpt.model import TransformerModel, AdversarialDiscriminator
 from scgpt.tokenizer import tokenize_and_pad_batch, random_mask_value
-from scgpt.loss import (
+from pretraining.loss import (
     masked_mse_loss,
     masked_relative_error,
     criterion_neg_log_bernoulli,
 )
-from scgpt.tokenizer.gene_tokenizer import GeneVocab
-from scgpt.preprocess import Preprocessor
+from pretraining.gene_tokenizer import GeneVocab
+from pretraining.preprocess import Preprocessor
 from scgpt import SubsetsBatchSampler
 from scgpt.utils import set_seed, category_str2int, eval_scib_metrics
 
